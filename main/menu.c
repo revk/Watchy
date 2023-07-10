daf7a8993bef591d8edfeb38c34582ae565f2331
RevK <github@k.gg>
2023-07-10 18:03:39 +0100

    Library update

--- main/menu.c
+++ main/menu.c
@@ -5,16 +5,23 @@
 #include "gfx.h"
 #include "icons.h"
 
+RTC_NOINIT_ATTR uint8_t btnlast;
+
+void gfx_menu(void)
+{ // Start menu
+}
+
 void
 menu_show (struct tm *t)
 {
-   ESP_LOGI ("Menu", "%X %d %d %d", bits.buttons, menu1, menu2, menu3);
-   if (bits.buttons == 9)
-   {                            // TODO force upgrade
-      bits.holdoff = 1;
-      bits.wifi = 1;
-      if (!revk_link_down () && !revk_shutting_down (NULL))
-         revk_command ("upgrade", NULL);
-   } else
-      bits.holdoff = 0;
+	uint8_t btn=bits.buttons&~btnlast; // Pushed
+	btnlast=bits.buttons;
+	if(btn&&!menu1)
+	{ // Any button starting
+		menu1=1;menu2=0;menu3=0;
+	}
+	if(!menu2)
+	{ // Showing top level menu
+	}
 }
+
