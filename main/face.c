285ac6508f39412a785bd21f31ec8eb2e8db4037
RevK <github@k.gg>
2023-07-11 13:33:46 +0100

    Comment

--- main/face.c
+++ main/face.c
@@ -98,7 +98,6 @@ face_basic (struct tm *t)
    gfx_qr (temp, 2);
    gfx_pos (199, 140, GFX_R | GFX_B | GFX_H);
    gfx_7seg (2, "%d", steps_read ());
-   // TODO walk/run icons?
    gfx_pos (199, 165, GFX_R | GFX_B | GFX_H);
    gfx_7seg (2, "%3d", battery);
    strftime (temp, sizeof (temp), "%a", t);
